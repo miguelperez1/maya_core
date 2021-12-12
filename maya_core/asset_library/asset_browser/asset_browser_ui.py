3442b2858edb1e39e19375c4166fcbf5cbd6b44e
Miguel Perez <36670243+miguelperez1@users.noreply.github.com>
2021-12-12 01:41:15 -0600

    = updates

--- maya_core/asset_library/asset_browser/asset_browser_ui.py
+++ maya_core/asset_library/asset_browser/asset_browser_ui.py
@@ -1,4 +1,5 @@
 import logging
+from functools import partial
 
 from PySide2 import QtCore
 from PySide2 import QtWidgets
@@ -9,6 +10,7 @@ import pymel.core as pm
 
 from tools_core.asset_library.asset_browser import AssetBrowserWidget
 from maya_core.maya_pyqt import MWidgets
+from tools_core.asset_library import library_manager as lm
 
 logger = logging.getLogger(__name__)
 logger.setLevel(10)
@@ -29,9 +31,48 @@ class ExampleDialog(QtWidgets.QMainWindow):
         self.create_widgets()
         self.create_layout()
         self.create_connections()
+        self.custom_browser_setup()
 
     def create_actions(self):
         pass
+
+    def create_custom_actions(self):
+        self.custom_actions = {}
+
+        for library in lm.LIBRARIES.keys():
+            if not lm.get_library_data(library):
+                continue
+
+            self.custom_actions[library] = {}
+
+        # Model library custom actions
+        model_action = QtWidgets.QAction("This is model")
+        material_action = QtWidgets.QAction("This is mtl")
+        after_action = QtWidgets.QAction("after")
+
+        self.custom_actions["model"] = [
+            {
+                "action_object": model_action,
+                "action_callback": ""
+            },
+            {
+                "action_object": "separator",
+                "action_callback": ""
+            },
+            {
+                "action_object": after_action,
+                "action_callback": ""
+            }
+        ]
+
+        self.custom_actions["material"] = [
+            {
+                "action_object": material_action,
+                "action_callback": ""
+            }
+        ]
+
+        self.asset_browser.add_actions_to_menus(self.custom_actions)
 
     def create_widgets(self):
         self.asset_browser = AssetBrowserWidget.AssetBrowserWidget()
@@ -54,6 +95,13 @@ class ExampleDialog(QtWidgets.QMainWindow):
         ]
 
         self.asset_browser.create_custom_connections(connections)
+
+    def create_custom_connections(self):
+        pass
+
+    def custom_browser_setup(self):
+        self.create_custom_connections()
+        self.create_custom_actions()
 
     def test_connection(self):
         print("hello")
